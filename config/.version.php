d3d06c8b4e446646046e9bd3c738f6166bbab251
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-14 18:57:50 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'License',
+    'description' => 'Web application edition license key',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-config-license/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
