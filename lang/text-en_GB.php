a7cd4fbced52542cb06b5dddb82c85baf902c257
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-14 18:57:04 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'License',
+    '{description}' => 'Web application edition license key',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Configuring license key']
+    ],
+
+    // Form: поля
+    'License key' => 'License key',
+    'Registered in the name' => 'Registered in the name',
+    'Date of registration' => 'Date of registration',
+    'Validity' => 'Validity',
+    'IP-address' => 'IP-address',
+    'Domain' => 'Domain',
+    'Phone' => 'Phone',
+    'Application edition' => 'Application edition',
+    'no limits' => 'no limits',
+    'from {0}' => 'from {0}',
+    'to {0}' => 'to {0}',
+    // Form: предупреждение
+    'Your license key is active, but there are {0} days left until your license expires' => 
+        'Your license key is <b>active</b>, but there are  <b>{0}</b> days left until your license expires',
+    'Your license key is not active, there are {0} days left until the license starts' =>
+        'Your license key is <b>not active</b>, there are <b>{0}</b> days left until the license starts',
+    'Your license key is not active' => 'Your license key is <b>not active</b>',
+    'Your license key is active' => 'Your license key is <b>active</b>',
+    'license key is not valid' => 'license key is <b>not valid</b>',
+    // Form: сообщения
+    'Error getting key' => 'Error getting key',
+    'Save settings' => 'Save settings',
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Лицензия',
+    '{description}' => 'Лицензионный ключ редакции веб-приложения',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Настройка лицензии']
+    ],
+
+    // Form: поля
+    'License key' => 'Лицензионный ключ',
+    'Registered in the name' => 'Зарегистрирован на имя',
+    'Date of registration' => 'Дата регистрации',
+    'Validity' => 'Период действия',
+    'IP-address' => 'IP-адрес',
+    'Domain' => 'Имя домена',
+    'Phone' => 'Телефон',
+    'Application edition' => 'Редакция веб-приложения',
+    'no limits' => 'без ограничений',
+    'from {0}' => 'c {0}',
+    'to {0}' => 'по {0}',
+    // Form: предупреждение
+    'Your license key is active, but there are {0} days left until your license expires' => 
+        'Ваш лицензионный ключ <b>активен</b>, но до окончания действия лицензии осталось <b>{0}</b> дней',
+    'Your license key is not active, there are {0} days left until the license starts' =>
+        'Ваш лицензионный ключ <b>не активен</b>, но до начала активации осталось <b>{0}</b> дней',
+    'Your license key is not active' => 'Ваш лицензионный ключ <b>не активен</b>',
+    'Your license key is active' => 'Ваш лицензионный ключ <b>активен</b>',
+    'license key is not valid' => 'лицензионный ключ <b>недействителен</b>',
+    // Form: сообщения
+    'Error getting key' => 'Ошибка получения ключа',
+    'Save settings' => 'Сохранение настроек',
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"'
+];
