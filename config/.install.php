483eafc1f2a02f4d7f55a14fbe805d7928c2a43e
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-14 18:57:43 +0300

    Файл конфигурации установки расширения

---
+++ config/.install.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации установки расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'id'          => 'rg.be.config.license',
+    'moduleId'    => 'rg.be.config',
+    'name'        => 'License',
+    'description' => 'Web application edition license key',
+    'namespace'   => 'Rg\Backend\Config\License',
+    'path'        => '/rg/rg.be.config.license',
+    'route'       => 'license',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'permissions' => ['any', 'info'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'RG Workspace'],
+        ['app', 'code' => 'RG CMS'],
+        ['app', 'code' => 'RG CRM'],
+        ['module', 'id' => 'rg.be.config']
+    ]
+];
